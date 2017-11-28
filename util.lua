dc894ecca82b79c9467a8eaf3825a9ee56c7ac9e
ichinii <sven.salt@yahoo.de>
2017-11-28 19:36:39 +0100

    fixed recursive table copy

--- util.lua
+++ util.lua
@@ -1,6 +1,7 @@
-function copy(obj)
+function copy(obj, seen)
   if type(obj) ~= 'table' then return obj end
-  local s = {}
+  if seen and seen[obj] then return seen[obj] end
+  local s = seen or {}
   local res = setmetatable({}, getmetatable(obj))
   s[obj] = res
   for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
